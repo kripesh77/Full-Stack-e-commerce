4cbc0204ef4ac9d37909adaf550ef78bf844b3cf
Kripesh <kg9773092+github@gmail.com>
2025-11-07 16:24:34 +0545

    fixed font sizes

--- frontend/src/ui/transition.jsx
+++ frontend/src/ui/transition.jsx
@@ -1,49 +1,55 @@
 //eslint-disable-next-line
 import { motion } from "framer-motion";
+import { useMobile } from "../hooks/useMediaQuery";
 
 //eslint-disable-next-line
 const transition = (OgComponent) => {
-  return () => (
-    <>
-      <OgComponent />
+  return () => {
+    const isMobile = useMobile();
+    const divCount = isMobile ? 2 : 5;
 
-      {/* Slide-in animation with 5 staggered divs */}
-      <div className="slide-in">
-        {[...Array(5)].map((_, i) => (
-          <motion.div
-            key={`slide-in-${i}`}
-            className="slide-in-child"
-            initial={{ scaleY: 0 }}
-            animate={{ scaleY: 0 }}
-            exit={{ scaleY: 1 }}
-            transition={{
-              duration: 0.8,
-              delay: (5 - i) * 0.05,
-              ease: [0.68, -0.6, 0.32, 1.6],
-            }}
-          />
-        ))}
-      </div>
+    return (
+      <>
+        <OgComponent />
 
-      {/* Slide-out animation with 5 staggered divs */}
-      <div className="slide-out">
-        {[...Array(5)].map((_, i) => (
-          <motion.div
-            key={`slide-out-${i}`}
-            className="slide-out-child"
-            initial={{ scaleY: 1 }}
-            animate={{ scaleY: 0 }}
-            exit={{ scaleY: 0 }}
-            transition={{
-              duration: 0.8,
-              delay: (5 - i) * 0.05 + 0.2,
-              ease: [0.68, -0.6, 0.32, 1.6],
-            }}
-          ></motion.div>
-        ))}
-      </div>
-    </>
-  );
+        {/* Slide-in animation with staggered divs */}
+        <div className="slide-in">
+          {[...Array(divCount)].map((_, i) => (
+            <motion.div
+              key={`slide-in-${i}`}
+              className="slide-in-child"
+              initial={{ scaleY: 0 }}
+              animate={{ scaleY: 0 }}
+              exit={{ scaleY: 1 }}
+              transition={{
+                duration: 0.8,
+                delay: (divCount - i) * 0.05,
+                ease: [0.68, -0.6, 0.32, 1.6],
+              }}
+            />
+          ))}
+        </div>
+
+        {/* Slide-out animation with staggered divs */}
+        <div className="slide-out">
+          {[...Array(divCount)].map((_, i) => (
+            <motion.div
+              key={`slide-out-${i}`}
+              className="slide-out-child"
+              initial={{ scaleY: 1 }}
+              animate={{ scaleY: 0 }}
+              exit={{ scaleY: 0 }}
+              transition={{
+                duration: 0.8,
+                delay: (divCount - i) * 0.05 + 0.2,
+                ease: [0.68, -0.6, 0.32, 1.6],
+              }}
+            ></motion.div>
+          ))}
+        </div>
+      </>
+    );
+  };
 };
 
 export default transition;
